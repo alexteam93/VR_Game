20a2d7aeed81425677685034fa8f89cb9586c2b2
Ivan <ivan06dreamer@gmail.com>
2020-07-12 14:04:02 +0300

    try merge 2

---
+++ Assets/Bokuemskiy/Scripts/ParticleSpawner.cs
@@ -0,0 +1,26 @@
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class ParticleSpawner : MonoBehaviour
+    {
+    public ParticleSystem Particle;
+
+    public int hp = 10;    
+    // void OnTriggerEnter (Collider other){
+    // hp--;
+    // ParticleSystem ParticleSpawned = Instantiate(Particle, this.transform);        
+    // ParticleSpawned.Play();
+    // }
+    void OnCollisionEnter (Collision other){
+    hp--;
+    Debug.Log("touched");
+    ParticleSystem ParticleSpawned = Instantiate(Particle, other.contacts[0].point, Quaternion.identity);        
+    ParticleSpawned.Play();
+    }
+    void Update(){
+    if (hp<=0){
+    Destroy(this.gameObject);
+    }
+    }
+    }
---
+++ Assets/Scripts/NoiseTrigger/NoiseTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class NoiseTrigger : MonoBehaviour
+{
+    public float alertLevel = 5f;
+    public UnityEngine.Events.UnityEvent alertEvent;
+    void OnCollisionEnter(Collision col)
+    {
+        float noiseLevel = col.relativeVelocity.magnitude;
+
+        if(noiseLevel > alertLevel)
+        {
+            alertEvent?.Invoke();
+        }
+
+    }
+}
